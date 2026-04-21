d909e3d02d4bf14c8985149c70ef452c6bf0892b
Kasia <jacynko@gmail.com>
2026-04-21 16:23:31 +0200

    update img hosting

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -84,59 +84,59 @@ export default function Projects() {
 
 const itemData = [
     {
-        img: 'https://firebasestorage.googleapis.com/v0/b/cirrus-kowalczyk.appspot.com/o/dom-ns.jpg?alt=media&token=f3b96696-6f83-481f-a6fe-a08c2eeb62b5',
+        img: 'https://f003.backblazeb2.com/file/cirrusmarekkowalczyk/dom-ns.jpg',
         title: 'Dom jednorodzinny, ul.Chruściela, Nowy Sącz',
     },
     {
-        img: 'https://firebasestorage.googleapis.com/v0/b/cirrus-kowalczyk.appspot.com/o/krakow-rakowiecka.jpg?alt=media&token=de4285e2-0cc6-40e8-9901-f83d3d9acfb5',
+        img: 'https://f003.backblazeb2.com/file/cirrusmarekkowalczyk/krakow-rakowiecka.jpg',
         title: 'Osiedle mieszkaniowe, ul.Rakowiecka, Kraków',
     },
     {
-        img: 'https://firebasestorage.googleapis.com/v0/b/cirrus-kowalczyk.appspot.com/o/RudaSlaska.jpg?alt=media&token=4223aa57-65ed-4f73-9e48-64f4cc556ee4',
+        img: 'https://f003.backblazeb2.com/file/cirrusmarekkowalczyk/RudaSlaska.jpg',
         title: 'Budynek mieszkalno-usługowy, Ruda Śląska',
     },
     {
-        img: 'https://firebasestorage.googleapis.com/v0/b/cirrus-kowalczyk.appspot.com/o/Rytro-przedszkole.jpg?alt=media&token=ff2ae939-edf0-4823-b4d1-a8cff2939f56',
+        img: 'https://f003.backblazeb2.com/file/cirrusmarekkowalczyk/Rytro-przedszkole.jpg',
         title: 'Przedszkole, Rytro',
     },
     {
-        img: 'https://firebasestorage.googleapis.com/v0/b/cirrus-kowalczyk.appspot.com/o/bierun-atut.jpg?alt=media&token=2c8f94c6-ad54-4ae8-9de9-df53fea54692',
+        img: 'https://f003.backblazeb2.com/file/cirrusmarekkowalczyk/bierun-atut.jpg',
         title: 'C.H. Atut, Bieruń',
     },
     {
-        img: 'https://firebasestorage.googleapis.com/v0/b/cirrus-kowalczyk.appspot.com/o/chorow-karczma-elewacja.jpg?alt=media&token=593de78e-14f7-4afc-873e-3916b32ed27c',
+        img: 'https://f003.backblazeb2.com/file/cirrusmarekkowalczyk/chorow-karczma-elewacja.jpg',
         title: 'Karczma, lunapark Legendia, Chorzów',
     },
     {
-        img: 'https://firebasestorage.googleapis.com/v0/b/cirrus-kowalczyk.appspot.com/o/chorzow-stacja-paliw.jpg?alt=media&token=7c8da99a-552d-47ea-b76f-e8c9eccfe7c6',
+        img: 'https://f003.backblazeb2.com/file/cirrusmarekkowalczyk/chorzow-stacja-paliw.jpg',
         title: 'Stacja paliw, Chorzów',
     },
     {
-        img: 'https://firebasestorage.googleapis.com/v0/b/cirrus-kowalczyk.appspot.com/o/czarna-owca-hotel-piwniczna.JPG?alt=media&token=d583e5a7-fd53-438b-869f-b58c33b12d52',
+        img: 'https://f003.backblazeb2.com/file/cirrusmarekkowalczyk/czarna-owca-hotel-piwniczna.JPG',
         title: 'Hotel Piwniczna SPA&Conference, Piwniczna - Zdroj',
     },
     {
-        img: 'https://firebasestorage.googleapis.com/v0/b/cirrus-kowalczyk.appspot.com/o/palacyk-ns.jpg?alt=media&token=d91ceacd-d28a-4dfe-96d9-d38487dac80b',
+        img: 'https://f003.backblazeb2.com/file/cirrusmarekkowalczyk/palacyk-ns.jpg',
         title: 'Pałacyk, Nowy Sącz',
     },
     {
-        img: 'https://firebasestorage.googleapis.com/v0/b/cirrus-kowalczyk.appspot.com/o/drukarnia-flexergis.jpg?alt=media&token=8ca94fc3-9ea1-423e-a7fb-d23b35733af8',
+        img: 'https://f003.backblazeb2.com/file/cirrusmarekkowalczyk/drukarnia-flexergis.jpg',
         title: 'Drukarnia Flexergis, Nowy Sącz',
     },
     {
-        img: 'https://firebasestorage.googleapis.com/v0/b/cirrus-kowalczyk.appspot.com/o/golkowice-biblioteka.jpg?alt=media&token=df9457b5-0798-4a97-afe6-e6e87e6a5884',
+        img: 'https://f003.backblazeb2.com/file/cirrusmarekkowalczyk/golkowice-biblioteka.jpg',
         title: 'Powiatowa i Miejsko-Gminna Biblioteka Publiczna im. Wiktora Bazielicha w Starym Sączu – Filia w Przysietnicy, Gołkowice Górne'
     },
     {
-        img: 'https://firebasestorage.googleapis.com/v0/b/cirrus-kowalczyk.appspot.com/o/krakow-pawia.jpg?alt=media&token=9403e6df-d407-411d-8322-abdfcf53f881',
+        img: 'https://f003.backblazeb2.com/file/cirrusmarekkowalczyk/krakow-pawia.jpg',
         title: 'Budynek usługowy, ul.Pawia, Kraków'
     },
     {
-        img: 'https://firebasestorage.googleapis.com/v0/b/cirrus-kowalczyk.appspot.com/o/mikolow-salwator.jpg?alt=media&token=71a5b4e7-b915-479e-ab81-b83925f158dd',
+        img: 'https://f003.backblazeb2.com/file/cirrusmarekkowalczyk/mikolow-salwator.jpg',
         title: 'Dom Seniora Salwator Park, Mikołów'
     },
     {
-        img: 'https://firebasestorage.googleapis.com/v0/b/cirrus-kowalczyk.appspot.com/o/piwniczanka.jpg?alt=media&token=99b4cc07-5a33-4592-952d-3d0a9ecd0305',
+        img: 'https://f003.backblazeb2.com/file/cirrusmarekkowalczyk/piwniczanka.jpg',
         title: ' Hala i budynek administracyjno-biurowy, ul.Zdrojowa, Piwniczna-Zdrój'
     }
 ]
